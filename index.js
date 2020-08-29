c07f6ebb18369e6421c5210cd22552ea58cb1302
Pål Hamel <pal.hamel@gmail.com>
2020-08-29 16:49:55 +0200

    add row data to sheet function

--- index.js
+++ index.js
@@ -13,17 +13,12 @@ const doc = new GoogleSpreadsheet('1CCks0Cuqqo8Iea9uJXMT-6Jgrdzg0Yf31WSQgD3yJus'
   console.log(doc.title);
   await doc.updateProperties({ title: 'renamed doc' });
   
-  // const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-  // console.log(sheet.title);
-  // console.log(sheet.rowCount);
-  
-  // // adding / removing sheets
-  // const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
-  // await newSheet.delete();
+  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
 
-
-
-
+  await sheet.addRows([
+  { title: 'UX design', location: 'Paris' },
+  { title: 'Tech lead', location: 'Oslo' },
+  ]);
 
 })()
  
