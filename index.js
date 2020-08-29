e96e06503a0c7a40287fc32c13761bbde78e3d96
Pål Hamel <pal.hamel@gmail.com>
2020-08-29 17:09:31 +0200

    extract to sheet.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+// const { GoogleSpreadsheet } = require('google-spreadsheet');
  
 // spreadsheet key is the long id in the sheets URL
-const doc = new GoogleSpreadsheet('1CCks0Cuqqo8Iea9uJXMT-6Jgrdzg0Yf31WSQgD3yJus');
+// const doc = new GoogleSpreadsheet('1CCks0Cuqqo8Iea9uJXMT-6Jgrdzg0Yf31WSQgD3yJus');
 
 (async function() {
 
 
-  // OR load directly from json file if not in secure environment
-  await doc.useServiceAccountAuth(require('./credentials.json'));
 
-  await doc.loadInfo(); // loads document properties and worksheets
   console.log(doc.title);
   await doc.updateProperties({ title: 'renamed doc' });
   
   const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
 
-  await sheet.addRows([
-  { title: 'UX design', location: 'Paris' },
-  { title: 'Tech lead', location: 'Oslo' },
-  ]);
+  // await sheet.addRows([
+  // { title: 'UX design', location: 'Paris' },
+  // { title: 'Tech lead', location: 'Oslo' },
+  // ]);
 
 })()
  
---
+++ sheet.js
@@ -0,0 +1,27 @@
+const { GoogleSpreadsheet } = require("google-spreadsheet");
+
+module.exports = class Sheet {
+  constructor() {
+    this.doc = new GoogleSpreadsheet(
+      "1CCks0Cuqqo8Iea9uJXMT-6Jgrdzg0Yf31WSQgD3yJus"
+    );
+  }
+  async load() {
+    // load directly from json file if not in secure environment
+    await this.doc.useServiceAccountAuth(require("./credentials.json"));
+    await this.doc.loadInfo(); // loads document properties and worksheets
+  }
+  async addRows(rows) {
+    const sheet = this.doc.sheetsByIndex[0]; 
+    await sheet.addRows(rows);
+  }
+}
+
+// (async function () {
+//   const sheet = new Sheet();
+//   await sheet.load();
+//   await sheet.addRows([
+//     { title: "UX design", location: "Paris" },
+//     { title: "Tech lead", location: "Oslo" },
+//   ]);
+// })();
